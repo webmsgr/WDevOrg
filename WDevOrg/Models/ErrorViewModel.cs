c33ceaea34fca377e6af97153569b59ef3248133
webmsgr <zapcontact000@gmail.com>
2024-10-12 14:15:05 -0700

    switch to .net identity

--- WDevOrg/Models/ErrorViewModel.cs
+++ WDevOrg/Models/ErrorViewModel.cs
@@ -1,8 +1,9 @@
-namespace WDevOrg.Models;
-
-public class ErrorViewModel
+namespace WDevOrg.Models
 {
-    public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
